b712a9314e4d0add32a2c63b70dd3e96b9e67a54
Kazys <kazys.stepanas@csiro.au>
2015-12-01 13:39:21 +1000

    Corrected downloads to use relative path.

--- ocurves/doc/docdownloads.h
+++ ocurves/doc/docdownloads.h
@@ -6,7 +6,7 @@
 
 This page lists the available download packages.
 
-The latest version for Windows 64-bit can be found here: <a href="http://kaznx.github.io/ocurves/downloads/OpenCurves-0.9.0-win64.msi">OpenCurves-0.9.0-win64</a>.
+The latest version for Windows 64-bit can be found here: <a href="./downloads/OpenCurves-0.9.0-win64.msi">OpenCurves-0.9.0-win64</a>.
 
 */
 
