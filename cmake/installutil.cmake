fcba45e58f7a5532ecfb798c6b4c72263541b794
Kazys <kazys.stepanas@csiro.au>
2015-12-18 12:03:50 +1000

    Adjusted Qt plugin installation to better support installing specific plugins or all plugins in the same directory as a reference plugin. This resolves issues with some platform specific plugins. Also changed installation script to better support installing runtime files to a subdirectory.

--- cmake/installutil.cmake
+++ cmake/installutil.cmake
@@ -3,53 +3,117 @@
 #==============================================================================
 include(CMakeParseArguments)
 
+
 #===============================================================================
+# A helper for install_qt5_plugins, setting up the installation of PLUGIN for
+# the specified CONFIG.
+#
+# 
+#===============================================================================
+function(_install_qt5_plugin PLUGIN CONFIG QTCONFIG DIRECTORY_INSTALL)
+  get_target_property(QPLUGIN_LOC Qt5::Q${PLUGIN}Plugin LOCATION_${QTCONFIG})
+  #message(":::::::::::::::::::::::::::::::::::::::::::::")
+  #message("Qt5::Q${PLUGIN}Plugin @ ${QPLUGIN_LOC}")
+  get_filename_component(QPLUGIN_DIR "${QPLUGIN_LOC}" DIRECTORY)
+  get_filename_component(QPLUGIN_SUBDIR "${QPLUGIN_DIR}" NAME)
+  if(NOT DIRECTORY_INSTALL)
+    install(FILES "${QPLUGIN_LOC}" DESTINATION "${PLUGIN_DEST_DIR}/${QPLUGIN_SUBDIR}" COMPONENT Runtime CONFIGURATIONS ${CONFIG})
+  else(NOT DIRECTORY_INSTALL)
+    # Here we will create a regular expression to match shared library files either with
+    # or without the expected debug library suffix. On Windows it's 'd', on MacOS it's "_debug"
+    # and probably so on Linux to.
+    unset(DEBUG_SUFFIX)
+    if(WIN32)
+      set(DEBUG_SUFFIX d)
+    else(WIN32)
+      set(DEBUG_SUFFIX _debug)
+    endif(WIN32)
+
+    # For non-debug change exclude the DEBUG_SUFFIX.
+    if(NOT CONFIG STREQUAL "Debug")
+      set(EXCLUDE EXCLUDE)
+    endif(NOT CONFIG STREQUAL "Debug")
+
+    # Set up the expected file extension. We strip any leading '.' character
+    # from CMAKE_SHARED_LIBRARY_SUFFIX because it's easier to create the
+    # regular expression that way.
+    set(LIB_SUFFIX ${CMAKE_SHARED_LIBRARY_SUFFIX})
+    if(LIB_SUFFIX)
+      string(SUBSTRING ${LIB_SUFFIX} 0 1 CHAR)
+      if(CHAR STREQUAL ".")
+        string(LENGTH ${LIB_SUFFIX} LEN)
+        math(EXPR LEN "${LEN} - 1")
+        string(SUBSTRING ${LIB_SUFFIX} 1 ${LEN} LIB_SUFFIX)
+      endif(CHAR STREQUAL ".")
+    endif(LIB_SUFFIX)
+
+    # Specify the matching expression. We expect:
+    #   <something>[suffix][version].{dll|so|dylib}
+    # where suffix may match or exclude _debug or d name suffixing.
+    set(LIB_REGEX "[^\\.]+${DEBUG_SUFFIX}\\.[0-9\\.]*${LIB_SUFFIX}")
+
+    install(DIRECTORY "${QPLUGIN_DIR}" DESTINATION "${PLUGIN_DEST_DIR}"
+            COMPONENT Runtime
+            CONFIGURATIONS ${CONFIG}
+            REGEX "${LIB_REGEX}" ${EXCLUDE}
+            PATTERN "*.lib" EXCLUDE
+            PATTERN "*.exp" EXCLUDE
+            PATTERN "*.ilk" EXCLUDE
+            PATTERN "*.pdb" EXCLUDE
+            )
+  endif(NOT DIRECTORY_INSTALL)
+endfunction(_install_qt5_plugin)
+
+#===============================================================================
+# install_qt5_plugins(PLUGIN_DEST_DIR plugin1 plugin2 ... dir pluginX)
+#
+# Sets up installation of a list of Qt plugins. In order to function, end
+# requested plugin must be defined as a valid target. This occurs normally when
+# using a find_package(Qt5<Module>) directive. The plugins for that module will
+# be defined. Some plugins may be omitted, in which case the entire plugin
+# directory may be marshalled based on a plugin which is defined as a target.
+# Adding 'dir' before the plugin name triggers this behaviour.
+#
+# For example, the Qt5::GUI plugins may be marshalled via the following
+# commands:
+#
+#       find_package(Qt5::GUI)
+#       install_qt5_plugins("plugins" dir MinimalIntegration)
+#
+# This uses the directory of Qt5::QMinimalIntegrationPlugin as the directory
+# to install.
 #===============================================================================
 function(install_qt5_plugins PLUGIN_DEST_DIR)
   CMAKE_PARSE_ARGUMENTS(IQP "" "" "" ${ARGN})
 
-  # Use the QMinimalIntegrationPlugin to find the plugins directory.
-  # Use qmake to find the binary directory.
-  get_target_property(QMINIMAL_LOC Qt5::QMinimalIntegrationPlugin LOCATION_Release)
-  get_filename_component(QT_PLUGINS_DIR "${QMINIMAL_LOC}" DIRECTORY)
-  get_filename_component(QT_PLUGINS_DIR "${QT_PLUGINS_DIR}" DIRECTORY)
+  set(DIRECTORY_INSTALL NO)
+  foreach(PLUGIN ${IQP_UNPARSED_ARGUMENTS})
+    if("${PLUGIN}" STREQUAL "dir")
+      set(DIRECTORY_INSTALL YES)
+    else("${PLUGIN}" STREQUAL "dir")
+      if(CMAKE_CONFIGURATION_TYPES)
+        foreach(CONFIG_TYPE ${CMAKE_CONFIGURATION_TYPES})
+          # Use Qt Debug only with a debug build. Release for all others.
+          if(CONFIG_TYPE STREQUAL "Debug")
+            set(CONFIG "Debug")
+          else(CONFIG_TYPE STREQUAL "Debug")
+            set(CONFIG "Release")
+          endif(CONFIG_TYPE STREQUAL "Debug")
+          _install_qt5_plugin(${PLUGIN} ${CONFIG_TYPE} ${CONFIG} ${DIRECTORY_INSTALL})
+        endforeach(CONFIG_TYPE)
+      else(CMAKE_CONFIGURATION_TYPES)
+        if(CMAKE_BUILD_TYPE)
+          set(CONFIG ${CMAKE_BUILD_TYPE})
+        else(CMAKE_BUILD_TYPE)
+          set(CONFIG Release)
+        endif(CMAKE_BUILD_TYPE)
 
-  if(NOT QT_PLUGINS_DIR)
-    # Cannot find, or no need to install plugins.
-    # This trips for Linux, where Qt will generally be installed in system folders.
-    return()
-  endif(NOT QT_PLUGINS_DIR)
+        _install_qt5_plugin(${PLUGIN} ${CONFIG} ${CONFIG} ${DIRECTORY_INSTALL})
+      endif(CMAKE_CONFIGURATION_TYPES)
 
-  if(UNIX AND NOT APPLE)
-    return()
-  endif(UNIX AND NOT APPLE)
-
-  if(WIN32)
-    set(DEBUG_REGEX "[^\\.]+d\\..+$")
-  else(WIN32)
-    set(DEBUG_REGEX "[^\\.]+_debug\\..+$")
-  endif(WIN32)
-
-  foreach(PLUGINDIR ${IQP_UNPARSED_ARGUMENTS})
-    if(CMAKE_CONFIGURATION_TYPES)
-      # Restrict installation in a multi-configuration setup.
-      install(DIRECTORY "${QT_PLUGINS_DIR}/${PLUGINDIR}" DESTINATION ${PLUGIN_DEST_DIR} COMPONENT Runtime
-              CONFIGURATIONS Release RelWithDebInfo MinSizeRel
-              REGEX "${DEBUG_REGEX}" EXCLUDE)
-      install(DIRECTORY "${QT_PLUGINS_DIR}/${PLUGINDIR}" DESTINATION ${PLUGIN_DEST_DIR} COMPONENT Runtime
-              CONFIGURATIONS Debug
-              REGEX "${DEBUG_REGEX}")
-    else(CMAKE_CONFIGURATION_TYPES)
-      # Single configuration. Only consider whether to use debug libraries or not.
-      if(CMAKE_BUILD_TYPE MATCHES "[Dd][Ee][Bb][Uu][Gg]")
-      install(DIRECTORY "${QT_PLUGINS_DIR}/${PLUGINDIR}" DESTINATION ${PLUGIN_DEST_DIR} COMPONENT Runtime
-              REGEX "${DEBUG_REGEX}")
-      else(CMAKE_BUILD_TYPE MATCHES "[Dd][Ee][Bb][Uu][Gg]")
-        install(DIRECTORY "${QT_PLUGINS_DIR}/${PLUGINDIR}" DESTINATION ${PLUGIN_DEST_DIR} COMPONENT Runtime
-                REGEX "${DEBUG_REGEX}" EXCLUDE)
-      endif(CMAKE_BUILD_TYPE MATCHES "[Dd][Ee][Bb][Uu][Gg]")
-    endif(CMAKE_CONFIGURATION_TYPES)
-  endforeach(PLUGINDIR)
+      set(DIRECTORY_INSTALL NO)
+    endif("${PLUGIN}" STREQUAL "dir")
+  endforeach(PLUGIN)
 endfunction(install_qt5_plugins)
 
 #===============================================================================
@@ -84,12 +148,14 @@ endfunction(target_runtime_dir)
 
 #===============================================================================
 # BUNDLE - treat as a bundle installation
+# BINDIR - the relative installation directory for the executable/applications.
+#     E.g, specify 'bin' if installing to a bin directory under the install path.
 # FIXDIRS - additional fixup directories
 # SEARCHDIRS - additional search directories
 # COMPONENT - install component. Default to Runtime
 #===============================================================================
 function(install_setup_pack TARGET)
-  CMAKE_PARSE_ARGUMENTS(ISP "QTCONF;BUNDLE" "COMPONENT" "FIXDIRS;SEARCHDIRS" ${ARGN})
+  CMAKE_PARSE_ARGUMENTS(ISP "QTCONF;BUNDLE" "COMPONENT" "BINDIR;FIXDIRS;SEARCHDIRS" ${ARGN})
 
   if(NOT ISP_COMPONENT)
     set(ISP_COMPONENT Runtime)
@@ -97,8 +163,21 @@ function(install_setup_pack TARGET)
 
   #----------------------------------------------------------
   # Setup packing variables. Default to pack to bin directory
-  set(plugin_conf_dir bin)
-  set(qtconf_dest_dir bin)
+  if(NOT ISP_BINDIR)
+    if(WIN32)
+      set(ISP_BINDIR .)
+    else(WIN32)
+      set(ISP_BINDIR bin)
+    endif(WIN32)
+  endif(NOT ISP_BINDIR)
+  set(plugin_conf_dir .)
+  set(qtconf_dest_dir ${ISP_BINDIR})
+
+    set(app_dir_prefix)
+  if(ISP_BINDIR AND NOT "${ISP_BINDIR}" STREQUAL ".")
+    set(app_dir_prefix "${ISP_BINDIR}/")
+  endif(ISP_BINDIR AND NOT "${ISP_BINDIR}" STREQUAL ".")
+
   set(APP_LIST)
   # Handle multi configuration builds
   set(TARGET_NAME "${TARGET}")
@@ -109,7 +188,7 @@ function(install_setup_pack TARGET)
       if(NOT TARGET_NAME)
         set(TARGET_NAME ${TARGET})
       endif(NOT TARGET_NAME)
-      list(APPEND APP_LIST ${CONFIG} "\${CMAKE_INSTALL_PREFIX}/${TARGET_NAME}${CMAKE_EXECUTABLE_SUFFIX}")
+      list(APPEND APP_LIST ${CONFIG} "\${CMAKE_INSTALL_PREFIX}/${app_dir_prefix}${TARGET_NAME}${CMAKE_EXECUTABLE_SUFFIX}")
     endforeach(CONFIG)
   else(CMAKE_CONFIGURATION_TYPES)
     # Single configuration build
@@ -131,12 +210,6 @@ function(install_setup_pack TARGET)
     set(plugin_conf_dir ../Contents/MacOS)
     set(APP_LIST "\${CMAKE_INSTALL_PREFIX}/${BUNDLE_NAME}.app")
   endif(APPLE AND ISP_BUNDLE)
-  if(WIN32)
-    # For windows don't use a bin directory.
-    set(plugin_conf_dir .)
-    SET(qtconf_dest_dir .)
-    # message("APP_LIST: ${APP_LIST}")
-  endif(WIN32)
 
   #----------------------------------------------------------
   # install a qt.conf file
